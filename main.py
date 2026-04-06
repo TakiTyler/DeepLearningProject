c2979c7b2e6635bb4df5ec9158e964ec47b2fa3a
tyler tran <tyler.tran88@yahoo.com>
2026-04-06 09:58:04 -0700

    added load_csv helper function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from constants import * # paths to CSVs in here
 
+# helper function to print out CSVs for debugging purposes
 def debug_CSVs(csv: str, indexes: list = None):
     if not os.path.exists(csv): raise ValueError('Input a proper CSV file location.')
 
@@ -9,6 +10,16 @@ def debug_CSVs(csv: str, indexes: list = None):
     if indexes != None: print(df.loc[indexes])
     else: print(df)
 
-if __name__ == '__main__':
+# helper function to load CSVs for use in other functions
+def load_csv(csv: str, indexes: list = None):
+    if not os.path.exists(csv): raise ValueError('Input a proper CSV file location.')
 
-    debug_CSVs(INTER_VAL)
+    df = pd.read_csv(csv)
+    if indexes != None: return df.loc[indexes]
+    else: return df
+
+if __name__ == '__main__':
+    
+    df = load_csv(RAW_RECI)
+    print(df.columns)
+    
